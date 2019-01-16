36972c32d8c545d8991d250e90e83ab00ae76d40
Abhishek Arpure <aarpure@codeaurora.org>
2019-01-16 18:12:52 +0530

    Initial Lito configuration
    Base configuration for Lito target bring-up.
    
    Change-Id: Ie87a3ed65e578c05665f0fe10799de69d2b62b33

---
+++ AndroidBoard.mk
@@ -0,0 +1,105 @@
+LOCAL_PATH := $(call my-dir)
+
+#----------------------------------------------------------------------
+# Host compiler configs
+#----------------------------------------------------------------------
+SOURCE_ROOT := $(shell pwd)
+TARGET_HOST_COMPILER_PREFIX_OVERRIDE := prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.15-4.8/bin/x86_64-linux-
+TARGET_HOST_CC_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)gcc
+TARGET_HOST_CXX_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)g++
+TARGET_HOST_AR_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)ar
+TARGET_HOST_LD_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)ld
+
+#----------------------------------------------------------------------
+# Compile (L)ittle (K)ernel bootloader and the nandwrite utility
+#----------------------------------------------------------------------
+ifneq ($(strip $(TARGET_NO_BOOTLOADER)),true)
+
+# Compile
+include bootable/bootloader/edk2/AndroidBoot.mk
+
+$(INSTALLED_BOOTLOADER_MODULE): $(TARGET_EMMC_BOOTLOADER) | $(ACP)
+	$(transform-prebuilt-to-target)
+$(BUILT_TARGET_FILES_PACKAGE): $(INSTALLED_BOOTLOADER_MODULE)
+
+droidcore: $(INSTALLED_BOOTLOADER_MODULE)
+endif
+
+#----------------------------------------------------------------------
+# Compile Linux Kernel
+#----------------------------------------------------------------------
+ifeq ($(KERNEL_DEFCONFIG),)
+     KERNEL_DEFCONFIG := vendor/lito_defconfig
+endif
+
+ifeq ($(TARGET_KERNEL_SOURCE),)
+     TARGET_KERNEL_SOURCE := kernel
+endif
+
+DTC := $(HOST_OUT_EXECUTABLES)/dtc$(HOST_EXECUTABLE_SUFFIX)
+
+TARGET_KERNEL_MAKE_ENV := DTC_EXT=dtc$(HOST_EXECUTABLE_SUFFIX)
+TARGET_KERNEL_MAKE_ENV += DTC_OVERLAY_TEST_EXT=$$(pwd)/$(UFDT_APPLY_OVERLAY)
+#TARGET_KERNEL_MAKE_ENV += CONFIG_BUILD_ARM64_DT_OVERLAY=y
+TARGET_KERNEL_MAKE_ENV += HOSTCC=$(SOURCE_ROOT)/$(TARGET_HOST_CC_OVERRIDE)
+TARGET_KERNEL_MAKE_ENV += HOSTAR=$(SOURCE_ROOT)/$(TARGET_HOST_AR_OVERRIDE)
+TARGET_KERNEL_MAKE_ENV += HOSTLD=$(SOURCE_ROOT)/$(TARGET_HOST_LD_OVERRIDE)
+TARGET_KERNEL_MAKE_ENV += HOSTCFLAGS="-I$$(pwd)/kernel/msm-4.19/include/uapi -I/usr/include -I/usr/include/x86_64-linux-gnu -L/usr/lib -L/usr/lib/x86_64-linux-gnu"
+TARGET_KERNEL_MAKE_ENV += HOSTLDFLAGS="-L/usr/lib -L/usr/lib/x86_64-linux-gnu"
+
+KERNEL_LLVM_BIN := $(lastword $(sort $(wildcard $(shell pwd)/$(LLVM_PREBUILTS_BASE)/$(BUILD_OS)-x86/clang-4*)))/bin/clang
+
+include $(TARGET_KERNEL_SOURCE)/AndroidKernel.mk
+$(TARGET_PREBUILT_KERNEL): $(DTC)
+
+$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+#----------------------------------------------------------------------
+# Copy additional target-specific files
+#----------------------------------------------------------------------
+include $(CLEAR_VARS)
+LOCAL_MODULE       := vold.fstab
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.target.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := gpio-keys.kl
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_KEYLAYOUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qti
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+#----------------------------------------------------------------------
+# Radio image
+#----------------------------------------------------------------------
+ifeq ($(ADD_RADIO_FILES), true)
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+	$(call add-radio-file,radio/$(f)))
+endif
+
+#----------------------------------------------------------------------
+# extra images
+#----------------------------------------------------------------------
+include device/qcom/common/generate_extra_images.mk
---
+++ lito.mk
@@ -0,0 +1,149 @@
+# Temporary bring-up config -->
+ALLOW_MISSING_DEPENDENCIES := true
+
+BUILD_BROKEN_PHONY_TARGETS := true
+BUILD_BROKEN_DUP_RULES := true
+TEMPORARY_DISABLE_PATH_RESTRICTIONS := true
+export TEMPORARY_DISABLE_PATH_RESTRICTIONS
+
+BOARD_AVB_ENABLE := false
+
+BOARD_HAVE_BLUETOOTH := false
+BOARD_HAVE_QCOM_FM := false
+TARGET_DISABLE_PERF_OPTIMIATIONS := true
+TARGET_DISABLE_DISPLAY := true
+
+$(call inherit-product, device/qcom/common/common64.mk)
+# Temporary bring-up config <--
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Temporary bring-up config -->
+PRODUCT_SUPPORTS_VERITY := false
+# Temporary bring-up config <--
+
+###########
+# Target naming
+PRODUCT_NAME := lito
+PRODUCT_DEVICE := lito
+PRODUCT_BRAND := qti
+PRODUCT_MODEL := Lito for arm64
+
+
+###########
+# Initial bring-up flags
+# TODO: Delete once bring-up is complete
+
+TARGET_HAS_GENERIC_KERNEL_HEADERS := true
+TARGET_HAS_LOW_RAM := true
+# Enable flag to support slow emulated device
+TARGET_PRESIL_SLOW_BOARD := true
+
+TARGET_USES_AOSP := true
+TARGET_USES_AOSP_FOR_AUDIO := false
+TARGET_USES_QCOM_BSP := false
+
+###########
+#QMAA flags starts
+###########
+#QMAA global flag for modular architecture
+#true means QMAA is enabled for system
+#false means QMAA is disabled for system
+
+TARGET_USES_QMAA := true
+
+#QMAA tech team flag to override global QMAA per tech team
+#true means overriding global QMAA for this tech area
+#false means using global, no override
+
+TARGET_USES_QMAA_OVERRIDE_DISPLAY := false
+TARGET_USES_QMAA_OVERRIDE_AUDIO   := false
+TARGET_USES_QMAA_OVERRIDE_VIDEO   := false
+TARGET_USES_QMAA_OVERRIDE_CAMERA  := false
+TARGET_USES_QMAA_OVERRIDE_GFX     := false
+TARGET_USES_QMAA_OVERRIDE_WFD     := false
+TARGET_USES_QMAA_OVERRIDE_SENSORS := false
+
+###########
+#QMAA flags ends
+###########
+
+###########
+# Kernel configurations
+TARGET_KERNEL_VERSION := 4.19
+#Enable llvm support for kernel
+KERNEL_LLVM_SUPPORT := true
+
+###########
+# Target configurations
+
+QCOM_BOARD_PLATFORMS += lito
+
+TARGET_USES_QSSI := true
+
+TARGET_USES_QMAA := true
+###QMAA Indicator Start###
+
+#Full QMAA HAL List
+QMAA_HAL_LIST :=
+
+#Indicator for each enabled QMAA HAL for this target. Each tech team locally verified their QMAA HAL and ensure code is updated/merged, then add their HAL module name to QMAA_ENABLED_HAL_MODULES as an QMAA enabling completion indicator
+QMAA_ENABLED_HAL_MODULES :=
+
+###QMAA Indicator End###
+
+#Default vendor image configuration
+ENABLE_VENDOR_IMAGE := true
+
+# default is nosdcard, S/W button enabled in resource
+PRODUCT_CHARACTERISTICS := nosdcard
+
+BOARD_FRP_PARTITION_NAME := frp
+
+# Android EGL implementation
+PRODUCT_PACKAGES += libGLES_android
+
+-include hardware/qcom/display/config/lito.mk
+
+PRODUCT_PACKAGES += fs_config_files
+PRODUCT_PACKAGES += gpio-keys.kl
+
+# A/B related packages
+PRODUCT_PACKAGES += update_engine \
+    update_engine_client \
+    update_verifier \
+    bootctrl.lito \
+    brillo_update_payload \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+# Boot control HAL test app
+PRODUCT_PACKAGES_DEBUG += bootctl
+
+DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/lito/framework_manifest.xml
+
+DEVICE_MANIFEST_FILE := device/qcom/lito/manifest.xml
+DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
+
+
+# Kernel modules install path
+KERNEL_MODULES_INSTALL := dlkm
+KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTALL)/lib/modules
+
+#Enable full treble flag
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+PRODUCT_VENDOR_MOVE_ENABLED := true
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+BOARD_SYSTEMSDK_VERSIONS := 28
+BOARD_VNDK_VERSION := current
+TARGET_MOUNT_POINTS_SYMLINKS := false
+
+
+###################################################################################
+# This is the End of target.mk file.
+# Now, Pickup other split product.mk files:
+###################################################################################
+# TODO: Relocate the system product.mk files pickup into qssi lunch, once it is up.
+$(call inherit-product-if-exists, vendor/qcom/defs/product-defs/system/*.mk)
+$(call inherit-product-if-exists, vendor/qcom/defs/product-defs/vendor/*.mk)
+###################################################################################
