eb5a189b5d7f688a8e2de24914df5dc8bece4eab
Salendarsingh Gaud <sgaud@codeaurora.org>
2019-03-24 15:20:46 +0530

    lito : Setting KERNEL_MAKE_ENV to use x86_64-linux-glibc2.17-4.8
    Change-Id: I92d6f18e14f989542bfbe2fdee14be01c76a69e2

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 # Host compiler configs
 #----------------------------------------------------------------------
 SOURCE_ROOT := $(shell pwd)
-TARGET_HOST_COMPILER_PREFIX_OVERRIDE := prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.15-4.8/bin/x86_64-linux-
+TARGET_HOST_COMPILER_PREFIX_OVERRIDE := prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8/bin/x86_64-linux-
 TARGET_HOST_CC_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)gcc
 TARGET_HOST_CXX_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)g++
 TARGET_HOST_AR_OVERRIDE := $(TARGET_HOST_COMPILER_PREFIX_OVERRIDE)ar
