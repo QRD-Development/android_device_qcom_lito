be2067bf83da3aee8a867ccab76ab89f214d94f0
Neeraj Soni <neersoni@codeaurora.org>
2019-06-06 20:36:41 +0530

    lito: Enable file based encryption
    Add support for FBE and metadata encryption in fstab.
    Enable FBE in fstab.
    Split mounting of partitions as early and late mount.
    Start keymaster, vold and wait until the services are
    ready prior to mounting of userdata.
    Provide flag to override double encryption for external
    storage data.
    
    Change-Id: I32dc75c57d1c75fd891b486a057f0ef84ddfe1d4

--- lito.mk
+++ lito.mk
@@ -204,6 +204,10 @@ PRODUCT_PACKAGES += telephony-ext
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.enableadvancedscan=true
 
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.crypto.volume.filenames_mode = "aes-256-cts" \
+ro.crypto.allow_encrypt_override = true
+
 #----------------------------------------------------------------------
 # wlan specific
 #----------------------------------------------------------------------
