41691e8a836426e2e0ed4efa8fe3b67f8ee4b9ff
Salendarsingh Gaud <sgaud@codeaurora.org>
2019-06-12 18:02:41 +0530

    Enabling KERNEL_SD_LLVM_SUPPORT
    Change-Id: Ia7b1ec21057ca981e62053d09db40f9161be256e

--- lito.mk
+++ lito.mk
@@ -78,6 +78,8 @@ TARGET_USES_QMAA_OVERRIDE_SENSORS := true
 TARGET_KERNEL_VERSION := 4.19
 #Enable llvm support for kernel
 KERNEL_LLVM_SUPPORT := true
+#Enable sd-llvm support for kernel
+KERNEL_SD_LLVM_SUPPORT := true
 
 ###########
 # Target configurations
