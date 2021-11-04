e8ee39e3da9d89f1e7f2c8a1c2577b46fd067b14
merothh <merothh@gmail.com>
2021-11-04 12:07:41 +0530

    lito: Initial (recovery only) tree
    * LineageOS recovery boots and is functional
    
    Signed-off-by: Edward Wu <bluehome.wu@gmail.com>

---
+++ lineage_lito.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lito device
+$(call inherit-product, device/qcom/lito/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_lito
+PRODUCT_DEVICE := lito
+PRODUCT_BRAND := qti
+PRODUCT_MODEL := lito
+PRODUCT_MANUFACTURER := qualcomm
