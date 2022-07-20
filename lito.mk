a3bb7b9e7b0a1e11374aa1e2ce269542dd5000c8
Jake Weinstein <jake@aospa.co>
2022-07-20 10:07:19 -0500

    lito: Drop BOARD_SYSTEMSDK_VERSIONS := 30
    This is not necessary.
    
    Change-Id: Ifbc6e173e8fd2b2deff59c61840d88276999565f

--- lito.mk
+++ lito.mk
@@ -129,9 +129,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
     wfd \
     wlan
 
-# SDK
-BOARD_SYSTEMSDK_VERSIONS := 30
-
 # Sensors
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
