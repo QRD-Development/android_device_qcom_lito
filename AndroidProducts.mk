db4b50f5d20e3b70ecbe45f44f82843a66055ed0
ReallySnow <reallysnow233@gmail.com>
2023-11-09 00:44:50 +0800

    lito: Initial
    Change-Id: I57f3215d8b9f1d1de72a53c89af04527810946f3

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2023 Project Mia
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/mia_lito.mk
+
+COMMON_LUNCH_CHOICES := \
+    mia_lito-user \
+    mia_lito-userdebug
---
+++ lito.mk
@@ -0,0 +1,75 @@
+#
+# Copyright (C) 2023 Project Mia
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/qcom/lito
+
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service \
+    checkpoint_gc \
+    otapreopt_script \
+    update_engine \
+    update_engine_client \
+    update_engine_sideload \
+    update_verifier
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+# API
+BOARD_SHIPPING_API_LEVEL := 30
+BOARD_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Architecture
+TARGET_BOARD_PLATFORM := lito
+TARGET_BOOTLOADER_BOARD_NAME := lito
+
+# AVB
+BOARD_AVB_ENABLE := true
+
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# FM
+BOARD_HAVE_QCOM_FM := false
+
+# Init
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    fstab.qcom.ramdisk
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Partitions
+PRODUCT_BUILD_ODM_IMAGE := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# SDK
+BOARD_SYSTEMSDK_VERSIONS := 30
+
+# Storage
+PRODUCT_CHARACTERISTICS := nosdcard
